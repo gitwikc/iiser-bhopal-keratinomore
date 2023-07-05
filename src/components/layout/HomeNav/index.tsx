178c2510e022f466987532a4b419012a3b02e91c
Sattwik Sahu <sattwik21@iiserb.ac.in>
2023-07-05 23:57:24 +0530

    add: Animate icons and brand text

--- src/components/layout/HomeNav/index.tsx
+++ src/components/layout/HomeNav/index.tsx
@@ -1,47 +1,73 @@
+import { motion, Variants } from "framer-motion";
 import * as React from "react";
+import { BsFillShareFill, BsPersonHeart } from "react-icons/bs";
 import { FaDna } from "react-icons/fa";
-import { RiHandCoinFill } from "react-icons/ri";
 import { MdDescription } from "react-icons/md";
-import { BsPersonHeart, BsFillShareFill } from "react-icons/bs";
-import Nav from "./Nav";
+import { RiHandCoinFill } from "react-icons/ri";
 import "../styles/HomeNav.css";
+import Nav from "./Nav";
+
+const ulVariants: Variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const liVariants: Variants = {
+  hidden: { opacity: 0, x: 20 },
+  show: { opacity: 1, x: 0, transition: { duration: 0.3 } },
+  // scrolled: { opacity: 0, x: -20, transition: { duration: 0.3 } },
+};
 
 const HomeNav: React.FC = () => {
   return (
     <nav className="HomeNav">
-      <ul className="links">
-        <li className="group">
+      <motion.ul
+        variants={ulVariants}
+        initial="hidden"
+        animate="show"
+        className="nav__links"
+      >
+        <motion.li variants={liVariants} className="nav__link">
           <Nav
+            text="Attributors"
+            icon={<BsFillShareFill size={32} />}
             to="/attributions"
-            icon={<BsFillShareFill />}
-            text="Attributions"
           />
-        </li>
-        <li className="group">
+        </motion.li>
+        <motion.li variants={liVariants} className="nav__link">
           <Nav
-            to="/contribution"
-            icon={<RiHandCoinFill />}
             text="Contribution"
+            icon={<RiHandCoinFill size={36} />}
+            to="/contribution"
           />
-        </li>
-        <li className="group">
+        </motion.li>
+        <motion.li variants={liVariants} className="nav__link">
           <Nav
-            to="/description"
-            icon={<MdDescription size={32} />}
             text="Description"
+            icon={<MdDescription size={40} />}
+            to="/description"
           />
-        </li>
-        <li className="group">
-          <Nav to="/engineering" icon={<FaDna />} text="Engineering" />
-        </li>
-        <li className="group">
+        </motion.li>
+        <motion.li variants={liVariants} className="nav__link">
           <Nav
-            to="/human-practices"
-            icon={<BsPersonHeart />}
-            text="Human Practices"
+            text="Engineering Success"
+            icon={<FaDna size={32} />}
+            to="/engineering"
           />
-        </li>
-      </ul>
+        </motion.li>
+        <motion.li variants={liVariants} className="nav__link">
+          <Nav
+            text="Human Practices"
+            icon={<BsPersonHeart size={36} />}
+            to="/human-practices"
+          />
+        </motion.li>
+      </motion.ul>
     </nav>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,35 @@
+import { motion, Variants } from "framer-motion";
+
 import "./styles/index.css";
+
+const leftVariant: Variants = {
+  hidden: { opacity: 0, x: 30 },
+  show: { opacity: 1, x: 0 },
+};
+
+const rightVariant: Variants = {
+  hidden: { opacity: 0, x: -30 },
+  show: { opacity: 1, x: 0 },
+};
 
 const Home = () => {
   return (
     <div className="Home">
       <section className="brand">
-        <div className="content">
+        <motion.div
+          initial={{ opacity: 0, x: -40 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            ease: "linear",
+            duration: 1,
+            x: { ease: "linear", duration: 0.1 },
+          }}
+          className="content"
+        >
           <span>Kerati</span>
           <span>No</span>
           <span>More</span>
-        </div>
+        </motion.div>
       </section>
       <section className="prob_state">
         <section id="part1">Content in part 1</section>
