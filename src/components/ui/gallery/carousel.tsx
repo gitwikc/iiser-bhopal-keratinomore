934022fbe48b64517d0b61e839b2aa286b73623f
Sattwik Sahu <sattwik21@iiserb.ac.in>
2023-10-05 21:45:47 +0530

    fix: carousel installation

---
+++ src/components/ui/gallery/carousel.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Carousel as ResponsiveCarousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+interface CarouselProps {
+  items: JSX.Element[];
+}
+
+const Carousel: React.FC<CarouselProps> = ({ items }) => {
+  return (
+    <ResponsiveCarousel>
+      {items.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </ResponsiveCarousel>
+  );
+};
+
+export default Carousel;
+
